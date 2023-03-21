a96da494b39b070898594ef4aa5e2e3666ef1764
wonsub1332 <wonsub5491@naver.com>
2023-03-21 15:25:58 +0900

    second commit

---
+++ ch2_32180879_김원섭.py
@@ -0,0 +1,49 @@
+a = 100.23
+b = a * 100
+c = a * b
+
+print(a,b,c)
+
+s = "hello World"
+print(s)
+
+print(a==b)
+
+d=a 
+
+print(a==d)
+print(a,d)
+a= 100 
+print(a==d)
+print(a,d)
+
+x = 10
+y = 20
+
+print(x,y)
+
+x,y = y,x #튜플 처리
+
+print(x,y)
+
+a= 10
+b= 10
+
+print(id(a),id(b))
+print(a==b)
+print(a is b)
+
+a= 300
+b= 300
+
+print(id(a),id(b))
+print(a==b)
+print(a is b)
+
+c= "hello1234"
+d= "hello1234"
+
+print(id(c),id(d))
+print(c==d)
+print(c is d)
+
