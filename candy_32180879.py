92de59ba50b0845b3ab2280c1288bdad949ed899
wonsub1332 <wonsub5491@naver.com>
2023-03-28 13:53:33 +0900

    3_28

--- candy_32180879.py
+++ candy_32180879.py
@@ -1,6 +1,10 @@
 m=5000
 candy=120
-print(m//candy,"개")
-print("잔돈",m%candy,"원")
 
-print("검산 :",m//candy*candy,'+',m%candy,"=",m//candy*candy+m%candy)
+nCandy= m//candy
+chage= m%candy 
+
+print(nCandy,"개")
+print("잔돈",chage,"원")
+
+print("검산 :",nCandy*candy,'+',chage,"=",nCandy*candy+chage)
---
+++ mtb.py
@@ -0,0 +1,5 @@
+import math
+hight=math.sqrt(3**2 + 4**2)
+time= 10/20 + hight/10 + hight/30 + 8/20
+
+print("자전거 주행 시간은 총:",time)
