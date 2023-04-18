07188f3bd0bcdb5b2037eadcffb38f90f5b087d4
wonsub1332 <wonsub5491@naver.com>
2023-04-18 15:47:13 +0900

    04.18

---
+++ bigNumber.py
@@ -0,0 +1,9 @@
+a=int(input("첫 번째 정수 :"))
+b=int(input("두 번째 정수 :"))
+max=0
+if(a>b):
+    max=a
+else: 
+    max=b
+
+print("큰 수는 ",max)
---
+++ if_weight.py
@@ -0,0 +1,6 @@
+weight= float(input("짐의 무게는 얼마입니까? :"))
+if(weight > 20):
+    print("무거운 짐은 20,000원을 내셔야 합니다.")
+else:
+    print("짐에 대한 수수료는 없습니다.")
+print("감사합니다.")
